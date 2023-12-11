e176d0446eaf7237369ae685413770c3bd96d6df
Daggle24 <alejandro24.daniel@gmail.com>
2023-12-11 15:29:40 +0100

    used another library

--- __init__.py
+++ __init__.py
@@ -2,6 +2,13 @@ from rembg import remove
 from PIL import Image
 import torch
 import numpy as np
+from transparent_background import Remover
+import folder_paths as comfy_paths
+import os
+MODELS_DIR =  comfy_paths.models_dir
+rembg_dir = os.path.join(MODELS_DIR, 'rembg')
+model_dir = rembg_dir+os.sep+'latest_test.pth'
+remover = Remover(ckpt=model_dir) # default setting
 
 # Tensor to PIL
 def tensor2pil(image):
@@ -28,8 +35,9 @@ class ImageRemoveBackgroundRembg:
     CATEGORY = "image"
 
     def remove_background(self, image):
-        image = pil2tensor(remove(tensor2pil(image)))
-        return (image,)
+        image = tensor2pil(image)
+        out = pil2tensor(remover.process(image, type='white',threshold=0.1))
+        return (out,)
 
 
 # A dictionary that contains all nodes you want to export with their names
